d7ee921e4301917fb28e5b25e2fe3cda6f61c91c
Viacheslav <lan1547@ukr.net>
2021-12-14 12:08:38 +0100

    lesson 3 rework

--- Lesson_1.3.py
+++ Lesson_1.3.py
@@ -1,19 +1,15 @@
-n=2
-m=61
+n=10
+m=1430
 
 def checkTime(days, minutes):
     
-    days = ((days*24*60 - minutes) // 24*60)// (24*60)
-    hours = (days*24 - minutes // 60) // 60
+    minutesInTotal=(days-(days*24*60-minutes)//(60*24))*60*24-minutes
 
-    
+    timeHours = minutesInTotal//60
 
-   
+    timeMinutes=minutesInTotal-timeHours*60
 
-    #print('Time: ',str(timeHours).rjust(2,'0'), ':', str(timeMinutes).rjust(2,'0'))
-    print (days)
-    print (hours)
-
+    print('Time: ',str(timeHours).rjust(2,'0'), ':', str(timeMinutes).rjust(2,'0'))
     
 checkTime(n,m)
 
